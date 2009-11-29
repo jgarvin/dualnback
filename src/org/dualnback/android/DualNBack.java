10452169c5e67bceeb00cc99aeac40aee3ac7ae8
jgarvin <joseph.h.garvin@gmail.com>
2009-11-29 16:47:23 -0600

    Holy crap, we have a moving square! :D

--- src/org/dualnback/android/DualNBack.java
+++ src/org/dualnback/android/DualNBack.java
@@ -1,21 +1,55 @@
 package org.dualnback.android;
 
+import android.util.Log;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.LinearLayout;
+import android.view.View;
 import android.view.Gravity;
 import android.view.ViewGroup.LayoutParams;
 
 import org.dualnback.android.Grid;
 import android.widget.Button;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+class NextStimulus extends TimerTask
+{
+	private Grid grid;
+	private View to_update;
+
+	private int x;
+	
+	public NextStimulus(Grid to_light, View to_update) {
+		super();
+		
+		grid = to_light;
+		x = 0;
+	}
+	
+	public void run() {
+		x = (x + 1) % 3;
+		
+		grid.lightSquare(x, 0);
+		grid.postInvalidate();
+	}
+}
+
 public class DualNBack extends Activity
 {
+	private static final String TAG = "DualNBack";
+
+	private Timer stimulus_timer_;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+		Log.v(TAG, "Starting up...");
+		
         super.onCreate(savedInstanceState);
 		TextView tv = new TextView(this);
 		tv.setText("test1");
@@ -25,9 +59,8 @@ public class DualNBack extends Activity
 		topLayout.setGravity(Gravity.FILL);
 		topLayout.setWeightSum(1.0f);
 
-		Grid i = new Grid(this);
-
-		topLayout.addView(i, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 0.1f));
+		Grid grid = new Grid(this);
+		topLayout.addView(grid, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 0.1f));
 
 		LinearLayout buttonLayout = new LinearLayout(this);
 		buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
@@ -42,5 +75,8 @@ public class DualNBack extends Activity
 		
 		topLayout.addView(buttonLayout, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 0.9f));
 		setContentView(topLayout);
+
+		stimulus_timer_ = new Timer();
+		stimulus_timer_.scheduleAtFixedRate(new NextStimulus(grid, topLayout), 0, 1000);
     }
 }
--- src/org/dualnback/android/Grid.java
+++ src/org/dualnback/android/Grid.java
@@ -6,8 +6,15 @@ import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
 import android.content.Context;
 
+import android.util.Log;
+
 public class Grid extends View {
     private ShapeDrawable mDrawable;
+
+	private static final String TAG = "Grid";
+
+	private int lit_square_x;
+	private int lit_square_y;
 
     public Grid(Context context) {
         super(context);
@@ -19,11 +26,25 @@ public class Grid extends View {
 
         mDrawable = new ShapeDrawable(new RectShape());
         mDrawable.getPaint().setColor(0xff0000ff);
-        mDrawable.setBounds(x, y, x + width, y + height);
+        lightSquare(0, 0);
     }
+
+	public void lightSquare(int x, int y) {
+		lit_square_x = x;
+		lit_square_y = y;
+	}
 
     protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+
+		final int square_width = getWidth() / 3;
+		final int square_height = getHeight() / 3;
+		
+		int pos_x = lit_square_x * square_width;
+		int pos_y = lit_square_y * square_height;
+
+		mDrawable.setBounds(pos_x, pos_y, pos_x + square_width, pos_y + square_height);
+
         mDrawable.draw(canvas);
     }
 }
