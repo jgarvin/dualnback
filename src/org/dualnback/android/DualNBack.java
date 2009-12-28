7455f057fae225311cacf37eb20cf236306b54af
jgarvin <joseph.h.garvin@gmail.com>
2009-12-28 15:47:21 -0500

    Add new class despite java being #@$!ing stupid

--- src/org/dualnback/android/DualNBack.java
+++ src/org/dualnback/android/DualNBack.java
@@ -10,14 +10,10 @@ import android.view.View;
 import android.view.Gravity;
 import android.view.ViewGroup.LayoutParams;
 
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.Canvas;
-import android.content.Context;
-import android.graphics.drawable.shapes.RectShape;
-
 import org.dualnback.android.Grid;
 import org.dualnback.android.Pair;
 import org.dualnback.android.Stimulus;
+//import org.dualnback.android.FeedbackRuler;
 
 import android.widget.Button;
 
@@ -343,47 +339,6 @@ class Level extends Object
 	}
 }
 
-class FeedbackRuler extends View {
-	private static final String TAG = "FeedbackRuler";
-	private ShapeDrawable drawable_;
-	private Timer feedback_clear_;
-
-	public FeedbackRuler(Context context) {
-		super(context);
-
-		drawable_ = new ShapeDrawable(new RectShape());
-		drawable_.getPaint().setColor(0xff0000ff);
-	}
-
-	public void activate(boolean good) {
-		if(good)
-			drawable_.getPaint().setColor(0xff00ff00);
-		else
-			drawable_.getPaint().setColor(0xffff0000);
-
-		invalidate();
-
-		feedback_clear_ = new Timer();
-
-		feedback_clear_.schedule(new TimerTask() {
-			public void run() {
-				try {
-					drawable_.getPaint().setColor(0xff0000ff);
-					postInvalidate();
-				} catch(Exception e) {
-					Log.v(TAG, e.toString());
-				}
-			}
-		}, 210);
-	}
-
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-
-		drawable_.setBounds(0, 0, getWidth(), getHeight());
-        drawable_.draw(canvas);
-    }
-}
 
 public class DualNBack extends Activity
 {
---
+++ src/org/dualnback/android/FeedbackRuler.java
@@ -0,0 +1,47 @@
+import android.view.View;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.Canvas;
+import android.content.Context;
+import android.graphics.drawable.shapes.RectShape;
+
+public class FeedbackRuler extends View {
+	private static final String TAG = "FeedbackRuler";
+	private ShapeDrawable drawable_;
+	private Timer feedback_clear_;
+
+	public FeedbackRuler(Context context) {
+		super(context);
+
+		drawable_ = new ShapeDrawable(new RectShape());
+		drawable_.getPaint().setColor(0xff0000ff);
+	}
+
+	public void activate(boolean good) {
+		if(good)
+			drawable_.getPaint().setColor(0xff00ff00);
+		else
+			drawable_.getPaint().setColor(0xffff0000);
+
+		invalidate();
+
+		feedback_clear_ = new Timer();
+
+		feedback_clear_.schedule(new TimerTask() {
+			public void run() {
+				try {
+					drawable_.getPaint().setColor(0xff0000ff);
+					postInvalidate();
+				} catch(Exception e) {
+					Log.v(TAG, e.toString());
+				}
+			}
+		}, 210);
+	}
+
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+
+		drawable_.setBounds(0, 0, getWidth(), getHeight());
+        drawable_.draw(canvas);
+    }
+}
